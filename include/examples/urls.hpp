1a0bb19dfc08a29b8d9f9175a7f6c85945bd5b86
Ivan Kolosov <zackwag32@gmail.com>
2022-09-17 21:37:54 +0400

    Send request and read response (blocking mode)

--- include/examples/urls.hpp
+++ include/examples/urls.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-static const std::string kHost{"www.w3.org"};
-static const std::array kPaths{
-    "/TR/html401/html40.txt", "/TR/2002/REC-xhtml1-20020801/xhtml1.pdf",
-    "/TR/REC-html32.html",
-    "/TR/2000/REC-DOM-Level-2-Core-20001113/DOM2-Core.txt"};
+static const std::string kHost{"httpbin.org"};
+static const std::array kPaths{"/get?arg1=a", "/get?arg1=bb", "/get?arg1=ccc",
+                               "/get?arg1=dddd"};
--- src/io/blocking.cpp
+++ src/io/blocking.cpp
@@ -4,33 +4,38 @@
 
 #include <iostream>
 #include <string>
+#include <cstring>
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/errno.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 
 #include <fmt/core.h>
 
 namespace {
 
-static const int kDomain = PF_INET6;
+static const int kAddressFamily = AF_INET6;
+// PF_UNSPEC is another option
 static const int kSocketType = SOCK_STREAM;
-static const int kProtocol = 0;
-static const int kInvalidSocketDescriptor = -1;
+static const int kInvalidCode = -1;
+static const int kRecvBufSize = 1024;
 static const std::string kHTTPServiceName{"http"};
 
 void ThrowErrorWithErrno(const std::string &message) {
   int saved_errno = errno;
-  throw std::runtime_error{fmt::format("{}: errno {}", message, saved_errno)};
+  auto *description = std::strerror(saved_errno);
+  throw std::runtime_error{
+      fmt::format("{}: errno {} ({})", message, saved_errno, description)};
 }
 
 io::Addrinfo ResolveHost(const std::string &host) {
   struct addrinfo getaddrinfo_hints;
   getaddrinfo_hints.ai_flags = 0;
-  getaddrinfo_hints.ai_family = kDomain;
+  getaddrinfo_hints.ai_family = kAddressFamily;
   getaddrinfo_hints.ai_socktype = kSocketType;
-  getaddrinfo_hints.ai_protocol = kProtocol;
+  getaddrinfo_hints.ai_protocol = 0;
   getaddrinfo_hints.ai_addrlen = 0;
   getaddrinfo_hints.ai_addr = nullptr;
   getaddrinfo_hints.ai_canonname = nullptr;
@@ -54,6 +59,64 @@ io::Addrinfo ResolveHost(const std::string &host) {
   return io::Addrinfo(an_addrinfo);
 }
 
+void PrintAddresses(addrinfo *addrinfo_ptr) {
+  for (auto *cur = addrinfo_ptr; cur != nullptr; cur = cur->ai_next) {
+    char address_str[60];
+    for (int i = 0; i < 60; ++i) {
+      address_str[i] = 0;
+    }
+    void *addr;
+    if (cur->ai_family == AF_INET) {
+      sockaddr_in *ipv4 = reinterpret_cast<sockaddr_in *>(cur->ai_addr);
+      addr = static_cast<void *>(&(ipv4->sin_addr));
+    } else if (cur->ai_family == AF_INET6) {
+      sockaddr_in6 *ipv6 = reinterpret_cast<sockaddr_in6 *>(cur->ai_addr);
+      addr = static_cast<void *>(&(ipv6->sin6_addr));
+    }
+    inet_ntop(cur->ai_family, addr, address_str, sizeof(address_str));
+    std::cout << fmt::format("address {}\n", address_str);
+  }
+}
+
+void SendRequest(int socket, const std::string &host, const std::string &path) {
+  auto req = fmt::format(
+      "GET {} HTTP/1.1\r\nHost: {}\r\nUser-Agent: "
+      "kolosovi/coroutines\r\nConnection: close\r\nAccept: */*\r\n\r\n",
+      path, host);
+  std::cout << req << std::endl;
+  auto *buf = req.c_str();
+  auto bytes_to_send = req.size();
+  while (bytes_to_send > 0) {
+    auto bytes_sent = send(socket, buf, bytes_to_send, 0);
+    if (bytes_sent == kInvalidCode) {
+      ThrowErrorWithErrno(fmt::format("cannot send HTTP request to {}", path));
+    }
+    bytes_to_send -= bytes_sent;
+  }
+}
+
+int ReadResponse(int socket, const std::string &path) {
+  std::unique_ptr<char> buf(new char[kRecvBufSize]);
+  auto *raw_buf = buf.get();
+  int count = 0;
+  while (true) {
+    auto bytes_received = recv(socket, raw_buf, kRecvBufSize, 0);
+    if (bytes_received == kInvalidCode) {
+      ThrowErrorWithErrno(
+          fmt::format("cannot receive HTTP response for {}", path));
+    }
+    count += bytes_received;
+    if (bytes_received == 0) {
+      break;
+    }
+    for (int i = 0; i < bytes_received; i++) {
+      std::cout << raw_buf[i];
+    }
+  }
+  std::cout << std::endl;
+  return count;
+}
+
 }  // namespace
 
 namespace io {
@@ -62,9 +125,10 @@ int BlockingDownload(const std::string host, const std::string path) {
   std::cout << "fetching " << path << " from " << host << "\n";
   auto address = ResolveHost(host);
   auto raw_addrinfo = address.Get();
+  PrintAddresses(raw_addrinfo);
   auto a_socket = socket(raw_addrinfo->ai_family, raw_addrinfo->ai_socktype,
                          raw_addrinfo->ai_protocol);
-  if (a_socket == kInvalidSocketDescriptor) {
+  if (a_socket == kInvalidCode) {
     ThrowErrorWithErrno("failed to create a socket descriptor");
   }
   auto connect_code =
@@ -72,7 +136,10 @@ int BlockingDownload(const std::string host, const std::string path) {
   if (connect_code) {
     ThrowErrorWithErrno("connect failed");
   }
-  return a_socket;
+  SendRequest(a_socket, host, path);
+  auto bytes_received = ReadResponse(a_socket, path);
+  std::cout << "bytes received: " << bytes_received << std::endl;
+  return bytes_received;
 }
 
 }  // namespace io
